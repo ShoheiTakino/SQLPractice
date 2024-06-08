7be7b410f5b03773211fdb8f733d1fd510164878
ShoheiTakino <shohei0522penguin@gmail.com>
2024-06-08 09:17:04 +0900

    add sql practice work 4_7_1_5

--- chapter4/sqlPractice4_7_1.sql
+++ chapter4/sqlPractice4_7_1.sql
@@ -35,6 +35,31 @@ where 分類 = '3'
 and 数量 >= 2
 order by 日付, 数量 desc
 
--- 4
+-- 模範解答: 4
 SELECT 日付, 商品名, 単価, 数量, 注文金額 FROM 注文履歴
  WHERE 分類 = '3' AND 数量 >= 2 ORDER BY 日付, 数量 DESC
+
+ -- 5
+ select distinct 分類, 商品名, サイズ, 単価
+from 注文履歴
+where  分類 = '1'
+union
+select distinct 分類, 商品名, null, 単価
+from 注文履歴
+where  分類 = '2'
+union
+select distinct 分類, 商品名, null, 単価
+from 注文履歴
+where  分類 = '3'
+order by 1, 2
+
+-- 模範解答:5 --
+SELECT DISTINCT 分類, 商品名, サイズ, 単価
+  FROM 注文履歴 WHERE 分類 = '1'
+ UNION
+SELECT DISTINCT 分類, 商品名, NULL, 単価
+  FROM 注文履歴 WHERE 分類 = '2'
+ UNION
+SELECT DISTINCT 分類, 商品名, NULL, 単価
+  FROM 注文履歴 WHERE 分類 = '3'
+ ORDER BY 1, 2;
