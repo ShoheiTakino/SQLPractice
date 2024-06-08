05eb7689bbad08a7cf093e67f1d5af50e3e59c01
ShoheiTakino <shohei0522penguin@gmail.com>
2024-06-08 09:39:00 +0900

    add 4_7_2_4

--- chapter4/sqlPractice4_7_2.sql
+++ chapter4/sqlPractice4_7_2.sql
@@ -31,3 +31,23 @@ order by 値
 
 -- 模範解答:3
 SELECT 値 FROM 整数 INTERSECT SELECT 値 FROM 偶数;
+
+-- 問題: 4 --
+--自然数から整数を除外して結果を空にする方法--
+select 値 
+from 自然数
+except
+select 値
+from 整数
+order by 値
+
+--奇数と偶数で積集合で重複がない状態にして結果を空にする方法--
+select 値 
+from 奇数
+intersect
+select 値
+from 偶数
+order by 値
+
+-- 模範解答:4
+SELECT 値 FROM 奇数 INTERSECT SELECT 値 FROM 偶数;
