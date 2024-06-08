5bba74ecd5b40967f4c9465ff9a45489dc20b837
ShoheiTakino <shohei0522penguin@gmail.com>
2024-06-08 09:31:21 +0900

    add 4_7_2_2
    order byで指定しないと、テーブルと同じdbの値順にならなかった

--- chapter4/sqlPractice4_7_2.sql
+++ chapter4/sqlPractice4_7_2.sql
@@ -10,4 +10,13 @@ order by 値
 -- 模範解答:1
 SELECT 値 FROM 奇数 UNION SELECT 値 FROM 偶数;
 
+-- 問題: 2 --
+select 値 
+from 整数
+except
+select 値
+from 偶数
+order by 値
 
+-- 模範解答:2
+SELECT 値 FROM 整数 EXCEPT SELECT 値 FROM 偶数
