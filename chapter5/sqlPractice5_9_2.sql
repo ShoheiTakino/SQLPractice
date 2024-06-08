ca04c4122096fea4318ffbc511486bc093d4b2b7
ShoheiTakino <shohei0522penguin@gmail.com>
2024-06-08 11:28:42 +0900

    add 5_9_2_2

--- chapter5/sqlPractice5_9_2.sql
+++ chapter5/sqlPractice5_9_2.sql
@@ -23,3 +23,30 @@ UPDATE 回答者
              WHEN 'cn' THEN '中国'
              WHEN 'fr' THEN 'フランス'
              WHEN 'vn' THEN 'ベトナム' END;
+
+-- 問題: 2 -- (ちょいむずかった)
+select trim(メールアドレス) as メールアドレス,
+        case when 20 <= 年齢 and 年齢 < 30 then '20代'
+             when 30 <= 年齢 and 年齢 < 40 then '30代'
+             when 40 <= 年齢 and 年齢 < 50 then '40代'
+             when 50 <= 年齢 and 年齢 < 60 then '50代'
+        end
+        || '' ||
+        case 住居 
+             when 'C' then '集合住宅'
+             when 'D' then '戸建て'
+        end
+        as 属性
+from 回答者
+
+
+-- 模範解答:2
+SELECT TRIM(メールアドレス) AS メールアドレス,
+       CASE WHEN 年齢 >= 20 AND 年齢 < 30 THEN '20代'
+            WHEN 年齢 >= 30 AND 年齢 < 40 THEN '30代'
+            WHEN 年齢 >= 40 AND 年齢 < 50 THEN '40代'
+            WHEN 年齢 >= 50 AND 年齢 < 60 THEN '50代' END
+       || '：' ||
+       CASE 住居 WHEN 'D' THEN '戸建て'
+                WHEN 'C' THEN '集合住宅' END AS 属性
+  FROM 回答者
