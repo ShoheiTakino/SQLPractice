bf5f6ff44300add13b3f7cfb298cac697b1dabc1
ShoheiTakino <shohei0522penguin@gmail.com>
2024-06-09 10:09:27 +0900

    add 6_7_1_4

--- chapter6/sqlPractice6_7_1.sql
+++ chapter6/sqlPractice6_7_1.sql
@@ -33,3 +33,19 @@ group by 都市名
 SELECT 都市名, AVG(降水量), MIN(最高気温), MAX(最低気温) -- 模範解答間違ってて草
   FROM 都市別気象観測
  GROUP BY 都市名;
+
+  -- 問題: 4 --
+select 月,
+       round(avg(降水量)) as 各月の平均降水量,
+       max(最高気温) as 各月の最高気温,
+       min(最低気温) as 各月の最低気温
+from 都市別気象観測
+group by 月 
+order by 月
+
+-- 模範解答:4
+SELECT 月, AVG(降水量), AVG(最高気温), AVG(最低気温)
+  FROM 都市別気象観測
+ GROUP BY 月
+
+ 
