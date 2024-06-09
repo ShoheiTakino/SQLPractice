38f119e9ba136568d7cd7a0e0eec5908570d2a45
ShoheiTakino <shohei0522penguin@gmail.com>
2024-06-09 09:56:02 +0900

    add 6_7_1_2

--- chapter6/sqlPractice6_7_1.sql
+++ chapter6/sqlPractice6_7_1.sql
@@ -9,5 +9,14 @@ from 都市別気象観測
 SELECT SUM(降水量), AVG(最高気温), AVG(最低気温)
   FROM 都市別気象観測;
 
-  
+  -- 問題: 2 --
+select sum(降水量) as 東京の年間降水量,
+       round(avg(最高気温), 1) as 東京の最高気温平均,
+       round(avg(最低気温), 1) as 東京の最低気温平均
+from 都市別気象観測
+where 都市名 = '東京'
 
+-- 模範解答:2
+SELECT SUM(降水量), AVG(最高気温), AVG(最低気温)
+  FROM 都市別気象観測
+ WHERE 都市名 = '東京';
