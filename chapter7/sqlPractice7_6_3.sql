857561a5e4597a9b19304fb453a937cc6ab7c1b2
ShoheiTakino <shohei0522penguin@gmail.com>
2024-06-16 22:29:03 +0900

    add 7_6_3_2

--- chapter7/sqlPractice7_6_3.sql
+++ chapter7/sqlPractice7_6_3.sql
@@ -12,6 +12,29 @@ SELECT 飼育県, COUNT(個体識別番号)
   FROM 個体識別
  GROUP BY 飼育県;
 
- 
+問題: 2(復習する)
+select 飼育県 as 都道府県名,
+       個体識別番号,
+       case 雌雄コード 
+       when '1' then '雄'
+       when '2' then '雌'
+       end as 雌雄
+from 個体識別
+where 飼育県 in (
+select 飼育県
+from 頭数集計
+order by 頭数集計 desc
+offset 0 rows fetch next 3 rows only
+)
+
+-- 模範解答:2
+SELECT 飼育県 AS 都道府県名, 個体識別番号,
+  CASE 雌雄コード WHEN '1' THEN '雄'
+                WHEN '2' THEN '雌' END AS 雌雄
+  FROM 個体識別
+ WHERE 飼育県 IN (SELECT 飼育県 FROM 頭数集計
+                  ORDER BY 頭数 DESC
+                 OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY);
+
 
 */
