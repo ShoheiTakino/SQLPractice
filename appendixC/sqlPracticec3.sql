963e227cbcdaedb336d84b5820d41396aa836363
ShoheiTakino <shohei0522penguin@gmail.com>
2024-07-15 16:21:06 +0900

    add appendix_c_3_3

--- appendixC/sqlPracticec3.sql
+++ appendixC/sqlPracticec3.sql
@@ -10,6 +10,11 @@ select *
 from 口座
 where 残高 > 0
 
+-- 問題: 3 --
+select *
+from 口座
+where 口座番号 < '1000000'
+
 
 -- 模範解答:1 
 -- 問題: 1 --
@@ -17,5 +22,8 @@ SELECT * FROM 口座 WHRERE 口座番号 = '0037651'
 
 -- 問題: 2 --
 SELECT * FROM 口座 WHRERE 残高 > 0
+
+-- 問題: 3 --
+SELECT * FROM 口座 WHRERE 口座番号 < '1000000'
 
 */
