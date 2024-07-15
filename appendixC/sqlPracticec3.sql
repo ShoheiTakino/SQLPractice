3b56bc1efc1b1564d874488a5696af743a78cc18
ShoheiTakino <shohei0522penguin@gmail.com>
2024-07-15 16:23:35 +0900

    add appendix_c_3_4

--- appendixC/sqlPracticec3.sql
+++ appendixC/sqlPracticec3.sql
@@ -15,6 +15,10 @@ select *
 from 口座
 where 口座番号 < '1000000'
 
+-- 問題: 4 --
+select *
+from 口座
+where 更新日 <= '2023-12-31'
 
 -- 模範解答:1 
 -- 問題: 1 --
@@ -25,5 +29,9 @@ SELECT * FROM 口座 WHRERE 残高 > 0
 
 -- 問題: 3 --
 SELECT * FROM 口座 WHRERE 口座番号 < '1000000'
+
+-- 問題: 4 --
+SELECT * FROM 口座 WHRERE 更新日 < '2024-01-01'
+
 
 */
