b55173e8cdc4515d382e7497dc7f104636b90cf1
ShoheiTakino <shohei0522penguin@gmail.com>
2024-07-15 16:19:30 +0900

    add appendix_c_3_2

--- appendixC/sqlPracticec3.sql
+++ appendixC/sqlPracticec3.sql
@@ -5,10 +5,17 @@ select *
 from 口座
 where 口座番号 = '0037651'
 
+-- 問題: 2 --
+select *
+from 口座
+where 残高 > 0
+
 
 -- 模範解答:1 
 -- 問題: 1 --
 SELECT * FROM 口座 WHRERE 口座番号 = '0037651'
 
+-- 問題: 2 --
+SELECT * FROM 口座 WHRERE 残高 > 0
 
 */
