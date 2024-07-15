1a1889231d5d0890e73f595db7681e6e287e0e68
ShoheiTakino <shohei0522penguin@gmail.com>
2024-07-15 16:08:52 +0900

    add appendix_c_2_3

--- appendixC/sqlPracticec2.sql
+++ appendixC/sqlPracticec2.sql
@@ -8,6 +8,10 @@ from 口座
 select 口座番号
 from 口座
 
+-- 問題: 3 --
+select 口座番号, 残高
+from 口座
+
 -- 模範解答:1 
 -- 問題: 1 --
 SELECT 口座番号, 名義, 種別, 残高, 更新日 FROM 口座
@@ -15,7 +19,8 @@ SELECT 口座番号, 名義, 種別, 残高, 更新日 FROM 口座
 -- 問題: 2 --
 SELECT 口座番号 FORM 口座
 
-
+-- 問題: 3 --
+SELECT 口座番号, 残高 FORM 口座
 
 
 */
