a4b14edf4ee4715c585880aadda7b5a1f7e7aa58
ShoheiTakino <shohei0522penguin@gmail.com>
2024-07-15 17:36:35 +0900

    add appendix_c_4_4

--- appendixC/sqlPracticec4.sql
+++ appendixC/sqlPracticec4.sql
@@ -16,6 +16,15 @@ from 口座
 order by 残高 desc,
 口座番号 asc
 
+-- 問題: 4 --
+select *
+from 口座
+where 更新日 is not null
+order by 更新日
+offset 0 rows
+fetch next 10 rows only
+
+
 -- 模範解答:1 
 SELECT 口座番号, 名義, 種別, 残高, 更新日 FROM 口座 ORDER BY 口座番号
 
@@ -24,5 +33,9 @@ SELECT DISTINCT 名義 FROM 口座 ORDER BY 名義
 
 -- 問題: 3 --
 SELECT 口座番号, 名義, 種別, 残高, 更新日 FROM 口座 ORDER BY 4 desc, 1
+
+-- 問題: 4 --
+SELECT * FROM 口座 Owhere 更新日 is not null order by 更新日 OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY
+
 
 */
