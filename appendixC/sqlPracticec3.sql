d694e7d6e8863a2a4c9dcf0c78506c7364b53c7e
ShoheiTakino <shohei0522penguin@gmail.com>
2024-07-15 16:24:57 +0900

    add appendix_c_3_5

--- appendixC/sqlPracticec3.sql
+++ appendixC/sqlPracticec3.sql
@@ -20,6 +20,14 @@ select *
 from 口座
 where 更新日 <= '2023-12-31'
 
+-- 問題: 5 --
+select *
+from 口座
+where 残高 >= 1000000
+
+
+
+
 -- 模範解答:1 
 -- 問題: 1 --
 SELECT * FROM 口座 WHRERE 口座番号 = '0037651'
@@ -32,6 +40,9 @@ SELECT * FROM 口座 WHRERE 口座番号 < '1000000'
 
 -- 問題: 4 --
 SELECT * FROM 口座 WHRERE 更新日 < '2024-01-01'
+
+-- 問題: 5 --
+SELECT * FROM 口座 WHRERE 残高 >= 1000000
 
 
 */
